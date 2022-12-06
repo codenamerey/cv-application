e39c821083c9de58dc27fa628bf9020a1ff19bd3
Lee Ryan <2solimanleeryan@gmail.com>
2022-12-06 12:14:46 +0800

    Create App Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Block from "./components/Block";
+
 function App() {
   return (
     <div className="App">
-
+      <Block title="General Info" info={[["Name",["First Name", "Last Name"]], ["Year Of Birth", ["Kahapon Lang"]]]}/>
+      <Block title="Educational Info" info={[["Secondary",["Input School",]], ["Tertiary", ["Input School"]]]}/>
     </div>
   );
 }
